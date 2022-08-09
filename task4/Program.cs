c71e2f186c6018cbacbcc57cfdb6d677e30e2fd1
Kristina <kdf-kris1602@mail.ru>
2022-08-09 19:08:17 +0300

    создала папку с дз 4

---
+++ task4/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = 22;
+int b = 3;
+int c = 9;
+
+if (a > b) 
+{
+    if (a > c)
+    {
+        Console.WriteLine( a );
+    }
+    else if (c > b)
+    {
+        Console.WriteLine( c );
+    }
+    else 
+    {
+        Console.WriteLine( b );
+    }
+
+} else if (b > a) 
+{
+    if (b > c)
+    {
+        Console.WriteLine( b );
+    }
+    else 
+    {
+        Console.WriteLine( c );
+    }
+}
