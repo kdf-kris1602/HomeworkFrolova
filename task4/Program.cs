f4422f4fcd075f9d746547adf915188e0a60dffc
Kristina <kdf-kris1602@mail.ru>
2022-08-09 19:22:11 +0300

    создала папку с дз 4 исправила

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,30 +1,33 @@
-﻿int a = 22;
-int b = 3;
-int c = 9;
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
 
 if (a > b) 
 {
     if (a > c)
     {
-        Console.WriteLine( a );
+        Console.WriteLine("самое большое " + a );
     }
     else if (c > b)
     {
-        Console.WriteLine( c );
+        Console.WriteLine("самое большое " + c );
     }
     else 
     {
-        Console.WriteLine( b );
+        Console.WriteLine("самое большое " + b );
     }
 
 } else if (b > a) 
 {
     if (b > c)
     {
-        Console.WriteLine( b );
+        Console.WriteLine("самое большое " + b );
     }
     else 
     {
-        Console.WriteLine( c );
+        Console.WriteLine("самое большое " + c );
     }
 }
