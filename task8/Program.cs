0d2de17e1519f050e9d5b6151a74ca2a1e11ed2b
Kristina <kdf-kris1602@mail.ru>
2022-08-10 16:40:36 +0300

    создала папку с дз 8

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int count  = 1;
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+    Console.WriteLine(count );
+    count = count + 1;
+    }
+    else
+    {
+    count = count + 1;
+    }
+} 
