56bdb97489207a393607e444e3e26a59b6fedd11
Kristina <kdf-kris1602@mail.ru>
2022-08-15 16:54:10 +0300

    Добавила 15 дз

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите номер дня недели (1.2.3.4.5.6.7)");
+ 
+string dayOfAWeek = Console.ReadLine();
+if (dayOfAWeek.Equals("6") || dayOfAWeek == "7")
+{                                                   
+    Console.WriteLine("Выходной день");
+}
+else Console.WriteLine("Будний день");
