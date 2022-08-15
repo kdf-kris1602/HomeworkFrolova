998f743b84b7c43c0667a794eae1b1062fd2b19d
Kristina <kdf-kris1602@mail.ru>
2022-08-15 16:41:37 +0300

    Добавила 10 дз

---
+++ task10/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter some 3-x number ");
+string x = Console.ReadLine();
+threeInOneOut(x);
+
+        void threeInOneOut(string someInt)
+        {
+            if (someInt.Length == 3)
+            {
+                Console.WriteLine(someInt.Trim(someInt[0], someInt[2]));
+            }
+            else
+            {
+                Console.WriteLine("wrong data,try again");
+                someInt = Console.ReadLine();
+                threeInOneOut(someInt);
+            }
+        }
+
+
+
