d0d51a37f0cb8ed5a3b9268c6e8fc17149a15e03
Kristina <kdf-kris1602@mail.ru>
2022-08-21 17:52:42 +0300

    добавила 25 дз

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string[] a = Console.ReadLine().Split(", ");
+        foreach (var elem in a) 
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", a));        
+        }
+    }
+}
