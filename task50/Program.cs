c7d6c58d27ab796b6140381afd6d124cff7756cc
Kristina <kdf-kris1602@mail.ru>
2022-08-29 17:07:54 +0300

    добавила 7 дз

---
+++ task50/Program.cs
@@ -0,0 +1,65 @@
+﻿
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine(); 
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SearchArray(int[,] matr)
+{
+    Console.WriteLine("Введите координаты x и у, чтобы узнать какое число стоит на этом месте, х: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("y: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+
+    if (i < matr.GetLength(0) || j < matr.GetLength(1))
+    {     
+        Console.WriteLine( "в данных координатах находится число - " + matr[i, j]);
+    }
+    else 
+    {
+        Console.WriteLine("вы ввели неверные координаты");
+    }
+}
+
+void NumbersArray(int[,] matr)
+{
+    Console.WriteLine("Введите число, на проверку наличия его в массиве ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (number == matr[i, j])
+            {
+                Console.WriteLine( number + "стоит на позиции [" + i + ", " + j + "]" );
+            }  
+        }
+    }
+    Console.WriteLine(number + " такого числа в массве нет");
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+SearchArray(matrix);
+NumbersArray(matrix);
---
+++ task52/Program.cs
@@ -0,0 +1,36 @@
+﻿
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine(); 
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10);// [1;10)
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+double sum1 = (matrix[0,0] + matrix[1,0] + matrix[2,0]) / 3;
+double sum2 = (matrix[0,1] + matrix[1,1] + matrix[2,1]) / 3;
+double sum3 = (matrix[0,2] + matrix[1,2] + matrix[2,2]) / 3;
+double sum4 = (matrix[0,3] + matrix[1,3] + matrix[2,3]) / 3;
+
+Console.Write(sum1 + "; " + sum2 + "; " + sum3 + "; " + sum4);
+
