98d7edf57620eb58c9ff1c56bc49d8ae93dd1598
Kristina <kdf-kris1602@mail.ru>
2022-08-30 16:07:03 +0300

    добавила 8 дз

---
+++ task54/Program.cs
@@ -0,0 +1,48 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine(); 
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void OrderArrayLines(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      for (int k = 0; k < matr.GetLength(1) - 1; k++)
+      {
+        if (matr[i, k] < matr[i, k + 1])
+        {
+          int temp = matr[i, k + 1];
+          matr[i, k + 1] = matr[i, k];
+          matr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int[,] matrix = new int[3,4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+OrderArrayLines(matrix);
+PrintArray(matrix);
---
+++ task56/Program.cs
@@ -0,0 +1,64 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);// [1;10)
+        }
+    }
+}
+
+
+
+int[,] matrix = new int[4, 4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(matrix, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+
